8bf1e78acaef4b4aba14534725e321d64f3c8ad8
Amaresh kumar <amiy.1279@gmail.com>
2020-02-09 00:08:23 +0000

    Initial build

---
+++ src/com/hello/world/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hello.world;
+
+public class HelloWorld {
+
+	public static void main (String [] args)
+	{
+		for (int i=1; i<=6; i++)
+		{
+			System.out.println("Hello World " + i);
+		}
+	}
+}
